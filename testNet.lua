c00c0a749e86828aba4b3b588f516d43bc838165
Stepan Tulyakov <tulyakov@deneb1.(none)>
2016-09-05 15:33:51 +0200

    Correcte sbatch file

--- testNet.lua
+++ testNet.lua
@@ -13,19 +13,30 @@ include('../mc-cnn/StereoJoin1.lua')
 include('../mc-cnn/SpatialConvolution1_fw.lua')
 include('../mc-cnn/SpatialLogSoftMax.lua')
 
+mcCnnFst = dofile('CMcCnnFst.lua');
+dofile('CAddMatrix.lua')
+milWrapper = dofile('CMilWrapper.lua')
+utils = dofile('utils.lua');
 
-
-
-fnet = torch.load('../mc-cnn/net/net_kitti_fast_-a_train_all.t7', 'ascii')[1]
+fnet0 = torch.load('../mc-cnn/net/net_kitti_fast_-a_train_all.t7', 'ascii')[1]
 -- remove normalization and stereo
-fnet:remove(9)
-fnet:remove(8)
-print(fnet)
--- 
+fnet0:remove(9)
+fnet0:remove(8)
 
-img_w = 100
-disp_max = 10;
-hpatch = 4;
+--fnet0_param = fnet0:getParameters()
 
-_TR_NET_ = mcCnnFst.getMilNetDoubleBatch(img_w, disp_max, hpatch, fnet)
-print(_TR_NET_)
+fnet = mcCnnFst.get(4,64,3)
+--fnet_param = fnet:getParameters()
+
+fnet = utils.copynet(fnet, fnet0)
+
+inp = torch.rand(1,9,9)
+out1 = fnet:forward(inp)
+out2 = fnet0:forward(inp:cuda())
+out2 = out2:double()
+
+print(torch.all(torch.eq(torch.squeeze(out1),torch.squeeze(out2[{{},{5},{5}}]))))
+
+print(torch.squeeze(out1))
+print(torch.squeeze(out2[{{},{5},{5}}]))
+
