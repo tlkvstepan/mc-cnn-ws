db462aa184f2ee38f0a0df7ed570fa06c4e99b32
tlkvstepan <tulyakov.stepan@gmail.com>
2016-09-16 17:03:53 +0200

    Compute 1st and 2nd max in one module

--- CMilWrapper.lua
+++ CMilWrapper.lua
@@ -310,7 +310,7 @@ return Net
 
 end
 
-function milWrapper.getMaxNetDoubleBatch(img_w, disp_max, hpatch, max_order, max_r, fnet)  
+function milWrapper.getMaxNetDoubleBatch(img_w, disp_max, hpatch, dist_min, fnet)  
 
 local fNetRef = fnet:clone()
 local Net = nn.Sequential()
@@ -343,150 +343,21 @@ splitter:add(stream2)
 
 -- stream1 :
 stream1:add(nn.Narrow(1, disp_max+1, img_w - 2*hpatch - disp_max))
-stream1:add(nn.Replicate(2))
-stream1:add(nn.SplitTable(1))
-local stream1par2 = nn.ParallelTable()
-stream1:add(stream1par2)
-local stream1seq1 = nn.Sequential()
-stream1par2:add(stream1seq1)
-stream1seq1:add(nn.Max(2))
-local stream1seq2 = nn.Sequential()
-stream1par2:add(stream1seq2)
-stream1seq2:add(nn.MaxM(2,max_order, max_r))
-stream1seq1:add(nn.Unsqueeze(2))
-stream1seq2:add(nn.Unsqueeze(2))
-stream1:add(nn.JoinTable(2))
+stream1:add(nn.contrastMax2ndMax(dist_min))
 
 -- stream2 :
 stream2:add(nn.Narrow(2,1, img_w - 2*hpatch - disp_max))
 stream2:add(nn.Transpose({1,2}))
-stream2:add(nn.Replicate(2))
-stream2:add(nn.SplitTable(1))
-local stream2par2 = nn.ParallelTable()
-stream2:add(stream2par2)
-local stream2seq1 = nn.Sequential()
-stream2par2:add(stream2seq1)
-stream2seq1:add(nn.Max(2))
-local stream2seq2 = nn.Sequential()
-stream2par2:add(stream2seq2)
-stream2seq2:add(nn.MaxM(2,max_order, max_r))
-stream2seq1:add(nn.Unsqueeze(2))
-stream2seq2:add(nn.Unsqueeze(2))
-stream2:add(nn.JoinTable(2))
+stream2:add(nn.contrastMax2ndMax(dist_min))
 
+Net:add(nn.JoinTable(1))
+Net:add(nn.SplitTable(2))
 Net:add(nn.JoinTable(1))
 Net:add(nn.SplitTable(2))
 
 return Net
 end 
 
-function milWrapper.getEntropyNetDoubleBatch(img_w, disp_max, hpatch, fnet)  
-
-  local fNetRef = fnet:clone(); 
-
-  local tabH1 = img_w - 2*hpatch - disp_max;
-  local tabW1 = img_w - 2*hpatch;
-  local tabH2 = img_w - 2*hpatch - disp_max;
-  local tabW2 = img_w - 2*hpatch;
-
-  local Net = nn.Sequential()
--- pass ref and pos epipolar lines through feature net and normalize outputs
-local parFeatureNet = nn.ParallelTable()
-Net:add(parFeatureNet)
-fNetRef:add(nn.Squeeze(2))
-fNetRef:add(nn.Transpose({1,2}))
-fNetRef:add(nn.Normalize(2))
-local fNetPos = fNetRef:clone('weight','bias', 'gradWeight','gradBias');
-parFeatureNet:add(fNetRef)
-parFeatureNet:add(fNetPos)
-
--- compute cross products ref and pos
-Net:add(nn.MM(false, true))
-
--- mask wrong disparities
---local mask = torch.ones(img_w-2*hpatch, img_w-2*hpatch)*2  
---mask = torch.triu(torch.tril(mask,-1),-disp_max)
---mask = mask - 2;
---Net:add(nn.addMatrix(mask))
---Net:add(nn.Clamp(-1,1))
-
--- make two streams : one with matrix and another with its transpose
---splitter = nn.ConcatTable()
---Net:add(splitter)
---stream1 = nn.Sequential()
---stream2 = nn.Sequential()
---splitter:add(stream1)
---splitter:add(stream2)
-
--- stream1 :
--- mask 
-Net:add(nn.Narrow(1,disp_max+1, img_w - 2*hpatch - disp_max))
--- perform softmax on rows
-Net:add(nn.SplitTable(1))
-local par1 = nn.ParallelTable()
-Net:add(par1)
-for i = 1, tabH1 do 
-    local softmax = nn.Sequential()
-    -- we want to compute entropy only for valid values
-    par1:add(softmax)
-    softmax:add(nn.Narrow(1,i,disp_max))
-    softmax:add(nn.SoftMax()) 
-    softmax:add(nn.Unsqueeze(1))
-end
-Net:add(nn.JoinTable(1))
----- make entropys
-Net:add(nn.Replicate(2))
-Net:add(nn.SplitTable(1))
-local par2 = nn.ParallelTable()
-Net:add(par2)
-local seq1 = nn.Sequential()
-par2:add(seq1)
-seq1:add(nn.MulConstant(-1))
-seq1:add(nn.View(1,tabH1*disp_max))
-local seq2 = nn.Sequential()
-par2:add(seq2)
-seq2:add(nn.Log())
-seq2:add(nn.View(tabH1*disp_max,1))
-Net:add(nn.MM(false, false))
-Net:add(nn.MulConstant(1/(tabH1)))
-
--- stream2 :
---stream2:add(nn.Narrow(2,1, img_w - 2*hpatch - disp_max))
----- mask
---stream2:add(nn.Transpose({1,2}))
----- perform softmax on rows
---stream2:add(nn.SplitTable(1))
---local par1 = nn.ParallelTable()
---stream2:add(par1)
---for i = 1, tabH2 do 
---    local softmax = nn.Sequential()
---    par1:add(softmax)
---    softmax:add(nn.SoftMax()) 
---    softmax:add(nn.Unsqueeze(1))
---end
---stream2:add(nn.JoinTable(1))
----- make entropys
---stream2:add(nn.Replicate(2))
---stream2:add(nn.SplitTable(1))
---local par2 = nn.ParallelTable()
---stream2:add(par2)
---local seq1 = nn.Sequential()
---par2:add(seq1)
---seq1:add(nn.MulConstant(-1))
---seq1:add(nn.View(1,tabH2*tabW2))
---local seq2 = nn.Sequential()
---par2:add(seq2)
---seq2:add(nn.Log())
---seq2:add(nn.View(tabH2*tabW2,1))
---stream2:add(nn.MM(false, false))
---stream2:add(nn.MulConstant(1/(tabH2)))
---Net:add(nn.JoinTable(1))
-
--- add criterion (y should always be 1)
---criterion:add(nn.HingeEmbeddingCriterion(0.1))
-  
-  return Net
-end
 
 -- Function creates triplet network by combining 3 feature nets
 -- Input of network is table of 3 of 1 x patch_height x patch_width 
