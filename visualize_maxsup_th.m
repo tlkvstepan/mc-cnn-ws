fb74c16fe8e638225bc49cc3ad11297c7b4b8e36
tlkvstepan <tulyakov.stepan@gmail.com>
2016-10-14 15:01:12 +0200

    matlab visualization

--- visualize_maxsup_th.m
+++ visualize_maxsup_th.m
@@ -3,21 +3,27 @@ close all;
 % 1-pix occ
 figure;
 title('Occlusion threshold selection')
-occ0File = 'work/contrast-dprog-0/contrast-dprog-0-2016_10_12_19:14:48'
-occ1File = 'work/contrast-dprog-1/contrast-dprog-1-2016_10_12_20:05:40'
-occ2File = 'work/contrast-dprog-2/contrast-dprog-2-2016_10_12_21:00:18'
-occ4File = 'work/contrast-dprog-4/contrast-dprog-4-2016_10_12_22:04:40'
+occ0File = 'work/maxsupression-threshold-selection/contrast-max-0/contrast-max-0-2016_10_14_10:58:36'
+occ1File = 'work/maxsupression-threshold-selection/contrast-max-1/contrast-max-1-2016_10_14_11:15:20'
+occ2File = 'work/maxsupression-threshold-selection/contrast-max-2/contrast-max-2-2016_10_14_11:32:10'
+occ4File = 'work/maxsupression-threshold-selection/contrast-max-4/contrast-max-4-2016_10_14_11:49:13'
+occ8File = 'work/maxsupression-threshold-selection/contrast-max-8/contrast-max-8-2016_10_14_12:06:47'
+occ16File = 'work/maxsupression-threshold-selection/contrast-max-16/contrast-max-16-2016_10_14_12:26:16'
 
 logOcc0 = dlmread(occ1File, ' ', 1, 1)
 logOcc1 = dlmread(occ1File, ' ', 1, 1)
 logOcc2 = dlmread(occ2File, ' ', 1, 1)
 logOcc4 = dlmread(occ4File, ' ', 1, 1)
+logOcc8 = dlmread(occ8File, ' ', 1, 1)
+logOcc16 = dlmread(occ16File, ' ', 1, 1)
 
-plot(logOcc0(:,2)); hold on
-plot(logOcc1(:,2)); hold on
-plot(logOcc2(:,2)); hold on
-plot(logOcc4(:,2)); hold on
+plot(logOcc0(:,2),'-o'); hold on
+plot(logOcc1(:,2),'-+'); hold on
+plot(logOcc2(:,2),'-*'); hold on
+plot(logOcc4(:,2),'-v'); hold on
+plot(logOcc8(:,2),'-s'); hold on
+plot(logOcc16(:,2),'-d'); hold on
 
 ylabel('Test accuracy: % of pixels |d-d_{gt}|<3')
 xlabel('epoch#')
-legend({'Occ0', 'Occ1', 'Occ2', 'Occ4'})
+legend({'Occ0', 'Occ1', 'Occ2', 'Occ4', 'Occ8', 'Occ16'})
--- visualize_training_curves.m
+++ visualize_training_curves.m
@@ -61,3 +61,35 @@ plot(smooth(logContrastDprog(:,2),15)); hold on
 ylabel('Test accuracy: % of pixels |d-d_{gt}|<3')
 xlabel('epoch#')
 legend({'Train error', 'Test accuracy', 'Smoothed test accuracy' })
+
+% mil-dprog
+figure;
+title('mil-dprog net')
+logFile1 = 'work/mil-dprog/mil-dprog-2016_10_12_22:57:46'
+logFile2 = 'work/mil-dprog/mil-dprog-2016_10_13_22:58:17'
+logInfo = [dlmread(logFile1, ' ', 1, 1);dlmread(logFile2, ' ', 1, 1)]
+yyaxis left
+plot(logInfo(:,1)); hold on
+ylabel('Train error: max(0, -x^{(+)}+x^{(-)}+\mu)')
+yyaxis right
+plot(logInfo(:,2)); hold on
+plot(smooth(logInfo(:,2),15)); hold on
+ylabel('Test accuracy: % of pixels |d-d_{gt}|<3')
+xlabel('epoch#')
+legend({'Train error', 'Test accuracy', 'Smoothed test accuracy' })
+
+% mil-contrast-dprog
+figure;
+title('mil-contrast-dprog net')
+logFile1 = 'work/mil-contrast-dprog/mil-contrast-dprog-2016_10_13_10:58:04'
+logInfo = dlmread(logFile1, ' ', 1, 1);
+yyaxis left
+plot(logInfo(:,1)); hold on
+ylabel('Train error: max(0, -x^{(+)}+x^{(-)}+\mu)')
+yyaxis right
+plot(logInfo(:,2)); hold on
+plot(smooth(logInfo(:,2),15)); hold on
+ylabel('Test accuracy: % of pixels |d-d_{gt}|<3')
+xlabel('epoch#')
+legend({'Train error', 'Test accuracy', 'Smoothed test accuracy' })
+
