3526a48fd622895de135d0c1b021923be2e7e18e
tlkvstepan <tulyakov.stepan@gmail.com>
2016-10-28 10:18:18 +0200

    Visualize training curves function

--- visualize_training_curves.m
+++ visualize_training_curves.m
@@ -1,127 +1,18 @@
 close all;
 
-if false
+large_scale = true
+
+
+if large_scale 
     
-    % mil-max
-    figure;
-    title('mil-max net')
-    logMilMaxFile = 'work/mil-max/mil-max-2016_09_26_15:08:39'
-    logMilMax = dlmread(logMilMaxFile, ' ', 1, 1)
-    yyaxis left
-    plot(logMilMax(:,1)); hold on
-    ylabel('Train error: max(0, -x^{(+)}+x^{(-)}+\mu)')
-    yyaxis right
-    plot(logMilMax(:,2)); hold on
-    plot(smooth(logMilMax(:,2),15)); hold on
-    ylabel('Test accuracy: % of pixels |d-d_{gt}|<3')
-    xlabel('epoch#')
-    legend({'Train error', 'Test accuracy', 'Smoothed test accuracy' })
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%       Large scale learning with contrast-dprog KITTI and KITTI15
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    % contrast max
-    figure;
-    title('contrast-max net')
-    logContrastMaxFile = 'work/contrast-max/contrast-max-2016_09_28_09:58:58'
-    logContrastMax = dlmread(logContrastMaxFile, ' ', 1, 1);
-    yyaxis left
-    plot(logContrastMax(:,1)); hold on
-    ylabel('Train error: max(0, -x^{(+)}+x^{(-)}+\mu)')
-    yyaxis right
-    plot(logContrastMax(:,2)); hold on
-    plot(smooth(logContrastMax(:,2),15)); hold on
-    ylabel('Test accuracy: % of pixels |d-d_{gt}|<3')
-    xlabel('epoch#')
-    legend({'Train error', 'Test accuracy', 'Smoothed test accuracy' })
-
-    % mil-contrast-max
-    figure;
-    title('mil-contrast-max net')
-    logMilContrastMaxFile1 = 'work/mil-contrast-max/mil-contrast-max-2016_09_29_12:58:18'
-    logMilContrastMaxFile2 =  'work/mil-contrast-max/mil-contrast-max-2016_10_02_13:18:26'
-    logMilContrastMax = [dlmread(logMilContrastMaxFile1, ' ', 1, 1);...
-                 dlmread(logMilContrastMaxFile2, ' ', 1, 1)]
-    yyaxis left
-    plot(logMilContrastMax(:,1)); hold on
-    ylabel('Train error: max(0, -x^{(+)}+x^{(-)}+\mu)')
-    yyaxis right
-    plot(logMilContrastMax(:,2)); hold on
-    plot(smooth(logMilContrastMax(:,2),15)); hold on
-    ylabel('Test accuracy: % of pixels |d-d_{gt}|<3')
-    xlabel('epoch#')
-    legend({'Train error', 'Test accuracy', 'Smoothed test accuracy' })
-
-    % contrast-dprog
-    figure;
-    title('contrast-dprog net')
-    logContrastDprogFile1 = 'work/contrast-dprog/contrast-dprog-2016_10_12_22:53:01'
-    logContrastDprogFile2 = 'work/contrast-dprog/contrast-dprog-2016_10_14_14:32:52'
-    logContrastDprog = [dlmread(logContrastDprogFile1, ' ', 1, 1);dlmread(logContrastDprogFile2, ' ', 1, 1)]
-    yyaxis left
-    plot(logContrastDprog(:,1)); hold on
-    ylabel('Train error: max(0, -x^{(+)}+x^{(-)}+\mu)')
-    yyaxis right
-    plot(logContrastDprog(:,2)); hold on
-    plot(smooth(logContrastDprog(:,2),15)); hold on
-    ylabel('Test accuracy: % of pixels |d-d_{gt}|<3')
-    xlabel('epoch#')
-    legend({'Train error', 'Test accuracy', 'Smoothed test accuracy' })
-
-    % mil-dprog
-    figure;
-    title('mil-dprog net')
-    logFile1 = 'work/mil-dprog/mil-dprog-2016_10_12_22:57:46'
-    logFile2 = 'work/mil-dprog/mil-dprog-2016_10_13_22:58:17'
-    logFile3 = 'work/mil-dprog/mil-dprog-2016_10_15_19:21:07'
-    logFile4 = 'work/mil-dprog/mil-dprog-2016_10_16_12:17:58'
-    logFile5 = 'work/mil-dprog/mil-dprog-2016_10_17_21:06:35'
-    logFile6 = 'work/mil-dprog/mil-dprog-2016_10_18_09:54:57'
-    logFile6 = 'work/mil-dprog/mil-dprog-2016_10_18_09:54:57'
-    logInfo = [dlmread(logFile1, ' ', 1, 1); dlmread(logFile2, ' ', 1, 1);
-               dlmread(logFile3, ' ', 1, 1); dlmread(logFile4, ' ', 1, 1);
-               dlmread(logFile5, ' ', 1, 1); dlmread(logFile6, ' ', 1, 1)]
-    yyaxis left
-    plot(logInfo(:,1)); hold on
-    ylabel('Train error: max(0, -x^{(+)}+x^{(-)}+\mu)')
-    yyaxis right
-    plot(logInfo(:,2)); hold on
-    plot(smooth(logInfo(:,2),15)); hold on
-    ylabel('Test accuracy: % of pixels |d-d_{gt}|<3')
-    xlabel('epoch#')
-    legend({'Train error', 'Test accuracy', 'Smoothed test accuracy' })
-
-    % mil-contrast-dprog
-    figure;
-    title('mil-contrast-dprog net')
-    logFile1 = 'work/mil-contrast-dprog/mil-contrast-dprog-2016_10_13_10:58:04'
-    logFile2 = 'work/mil-contrast-dprog/mil-contrast-dprog-2016_10_14_12:50:15'
-    logFile3 = 'work/mil-contrast-dprog/mil-contrast-dprog-2016_10_15_19:16:25'
-    logFile4 = 'work/mil-contrast-dprog/mil-contrast-dprog-2016_10_19_14:30:19'
-    logFile5 = 'work/mil-contrast-dprog/mil-contrast-dprog-2016_10_20_11:49:47'
-    logFile6 = 'work/mil-contrast-dprog/mil-contrast-dprog-2016_10_21_13:56:51'
-    logFile7 = 'work/mil-contrast-dprog/mil-contrast-dprog-2016_10_21_18:58:39'
-    logInfo = [dlmread(logFile1, ' ', 1, 1);
-               dlmread(logFile2, ' ', 1, 1);
-               dlmread(logFile3, ' ', 1, 1);
-               dlmread(logFile4, ' ', 1, 1);
-               dlmread(logFile5, ' ', 1, 1);
-               dlmread(logFile6, ' ', 1, 1);
-               dlmread(logFile7, ' ', 1, 1)]
-    yyaxis left
-    plot(logInfo(:,1)); hold on
-    ylabel('Train error: max(0, -x^{(+)}+x^{(-)}+\mu)')
-    yyaxis right
-    plot(logInfo(:,2)); hold on
-    plot(smooth(logInfo(:,2),15)); hold on
-    ylabel('Test accuracy: % of pixels |d-d_{gt}|<3')
-    xlabel('epoch#')
-    legend({'Train error', 'Test accuracy', 'Smoothed test accuracy' })
-
-end
-
-% contrast max
 figure;
-title('contrast-dprog KITTI')
-logFile = 'work/contrast-dprog-kitti15/contrast-dprog-kitti15-2016_10_23_17:39:22'
-log = dlmread(logFile, ' ', 1, 1);
+file1 = 'work/contrast-dprog-kitti15/contrast-dprog-kitti15-2016_10_26_17:41:06'
+file2 = 'work/contrast-dprog-kitti15/contrast-dprog-kitti15-2016_10_27_09:48:48'
+log = [dlmread(file1, ' ', 1, 1); dlmread(file2, ' ', 1, 1)]
 yyaxis left
 plot(log(:,1)); hold on
 ylabel('Train error: max(0, -x^{(+)}+x^{(-)}+\mu)')
@@ -131,3 +22,21 @@ plot(smooth(log(:,2),15)); hold on
 ylabel('Test accuracy: % of pixels |d-d_{gt}|<3')
 xlabel('epoch#')
 legend({'Train error', 'Test accuracy', 'Smoothed test accuracy' })
+title('Large scale contrast-dprog KITTI15')
+
+figure;
+file1 = 'work/contrast-dprog-kitti/contrast-dprog-kitti-2016_10_26_17:41:03'
+file2 = 'work/contrast-dprog-kitti/contrast-dprog-kitti-2016_10_27_09:47:32'
+log = [dlmread(file1, ' ', 1, 1); dlmread(file2, ' ', 1, 1)]
+yyaxis left
+plot(log(:,1)); hold on
+ylabel('Train error: max(0, -x^{(+)}+x^{(-)}+\mu)')
+yyaxis right
+plot(log(:,2)); hold on
+plot(smooth(log(:,2),15)); hold on
+ylabel('Test accuracy: % of pixels |d-d_{gt}|<3')
+xlabel('epoch#')
+legend({'Train error', 'Test accuracy', 'Smoothed test accuracy' })
+title('Large scale contrast-dprog KITTI')
+
+end
